9948ba2f6b1c31b66ff50559ce4d089e5c0390ba
Ashley Sullins & teresa fletcher <student@epicodus.com>
2015-08-26 12:00:29 -0700

    pass first Restaurant unit test

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Restaurant {
+  private int id;
+  private int cuisine_id;
+  private String name;
+
+  public int getId(){
+    return id;
+  }
+
+  public int getCuisine_id(){
+    return cuisine_id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public Restaurant(String name, int cuisine_id) {
+    this.name = name;
+    this.cuisine_id = cuisine_id;
+  }
+
+  @Override
+  public boolean equals(Object otherRestaurant) {
+    if(!(otherRestaurant instanceof Restaurant)) {
+      return false;
+    } else {
+      Restaurant newRestaurant = (Restaurant) otherRestaurant;
+      return this.getName().equals(newRestaurant.getName()) &&
+             this.getId() == newRestaurant.getId() &&
+             this.getCuisine_id() == newRestaurant.getCuisine_id();
+       }
+      }
+
+    public static List<Restaurant> all() {
+      String sql ="SELECT id, name, cuisine_id FROM restaurant";
+      try(Connection con = DB.sql2o.open()) {
+        return con.createQuery(sql).executeAndFetch(Restaurant.class);
+      }
+    }
+}
---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+
+public class RestaurantTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void all_emptyAtFirst() {
+   assertEquals(Restaurant.all().size(), 0);
+  }
+
+}
